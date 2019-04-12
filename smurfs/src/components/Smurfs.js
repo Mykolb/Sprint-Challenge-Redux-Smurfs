02bb488d79f7294f82d23c0ea4880a2625a0f698
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-12 11:51:46 -0400

    added smurfs component

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Smurf from './Smurf';
+
+
+class Smurfs extends Component {
+    render() {
+     return(
+         <div>
+        <ul>
+        {this.props.smurfs.map(smurf => {
+            return(
+                <Smurf 
+                name={smurf.name}
+                age={smurf.age}
+                height={smurf.height}
+                id= {smurf.id}
+                key={smurf.id}
+                />
+            )
+        })}
+        </ul>
+            </div>
+     )
+    }
+}
