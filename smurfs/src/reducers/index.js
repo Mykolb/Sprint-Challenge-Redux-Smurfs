3718ebb00e3d89b5b89030c8ae6656ed6d81f240
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-12 11:41:02 -0400

    added reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,67 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+FETCH_SMURF,
+FETCH_SMURF_SUCCESS,
+FETCH_SMURF_FAILURE,
+CREATE_SMURF,
+CREATE_SMURF_SUCCESS,
+CREATE_SMURF_FAILURE
+} from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
+
+//  Your initial/default state for this project could *Although does not have to* look a lot like this
+ const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+   addingSmurf: false,
+  //  updatingSmurf: false
+  //  deletingSmurf: false
    error: null
- }
-*/
+}
+
+export const smurfReducer = (state = initialState, action) => {
+  console.log('smurfReducer', action);
+  switch (action.type) {
+    case FETCH_SMURF: {
+      return {
+        ...state,
+        fetchingSmurfs: true,
+      };
+    }
+      case FETCH_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, ...action.payload],
+        fetchingSmurfs: true,
+      };
+      case FETCH_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        fetchingSmurfs: false,
+      };
+      case CREATE_SMURF:
+      return {
+        ...state,
+        addingSmurf: true
+      };
+      case CREATE_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, ...action.payload],
+        fetchingSmurfs: false
+      };
+      case CREATE_SMURF_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    }
+  }
+
+export default smurfReducer;
 
 /*
   You'll only need one smurf reducer for this project.
